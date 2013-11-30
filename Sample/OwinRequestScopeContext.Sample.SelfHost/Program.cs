a5396dbb7651cc0aafe666c15d83116ac63d3142
neuecc <ils@neue.cc>
2013-11-30 20:54:27 +0900

    done construct OwinRequestScopeContext

--- Sample/OwinRequestScopeContext.Sample.SelfHost/Program.cs
+++ Sample/OwinRequestScopeContext.Sample.SelfHost/Program.cs
@@ -1,15 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Threading;
 using System.Threading.Tasks;
 
-namespace OwinRequestScopeContext.Sample.SelfHost
+namespace Sample.SelfHost
 {
     class Program
     {
         static void Main(string[] args)
         {
+            using (WebApp.Start<Startup>("http://localhost:12345"))
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(Owin.IAppBuilder app)
+        {
+            app.UseRequestScopeContext();
+
+            app.Run(async ctx =>
+            {
+                var _ = OwinRequestScopeContext .Current;
+                var tid = Thread.CurrentThread.ManagedThreadId;
+
+                ctx.Response.ContentType = "text/plain";
+                await ctx.Response.WriteAsync("hello");
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+
+                var tid2 = Thread.CurrentThread.ManagedThreadId;
+
+                var __ = OwinRequestScopeContext.Current;
+
+                Console.WriteLine(__);
+            });
         }
     }
 }
