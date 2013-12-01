0945b20d7fde117f8f443e4e253ff9f7e93aa2da
neuecc <ils@neue.cc>
2013-12-01 20:20:23 +0900

    add OwinRequestScopeContextTest

---
+++ Tests/OwinRequestScopeContext.Test/OwinRequestScopeContextTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Owin.Test
+{
+    [TestClass]
+    public class OwinRequestScopeContextTest
+    {
+        [TestMethod]
+        public async Task Current()
+        {
+            var blankEnvironment = new Dictionary<string, object>();
+
+            OwinRequestScopeContext.Current = new OwinRequestScopeContext(blankEnvironment, true);
+
+            OwinRequestScopeContext.Current.Items["test"] = 10;
+            OwinRequestScopeContext.Current.Items["test2"] = 100;
+
+            await Task.Delay(TimeSpan.FromMilliseconds(10));
+
+            // transparent to await
+            OwinRequestScopeContext.Current.IsNotNull();
+            OwinRequestScopeContext.Current.Items["test"].Is(10);
+            OwinRequestScopeContext.Current.Items["test2"].Is(100);
+
+            await Task.Delay(TimeSpan.FromMilliseconds(10)).ConfigureAwait(false);
+
+            OwinRequestScopeContext.Current.IsNotNull();
+            OwinRequestScopeContext.Current.Items["test"].Is(10);
+            OwinRequestScopeContext.Current.Items["test2"].Is(100);
+
+            // run another thread
+            var t = Task.Run(() =>
+            {
+                OwinRequestScopeContext.Current.IsNotNull();
+                OwinRequestScopeContext.Current.Items["test"].Is(10);
+                OwinRequestScopeContext.Current.Items["test2"].Is(100);
+            });
+
+            await t;
+
+            var semaphore = new SemaphoreSlim(1);
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                OwinRequestScopeContext.Current.IsNotNull();
+                OwinRequestScopeContext.Current.Items["test"].Is(10);
+                OwinRequestScopeContext.Current.Items["test2"].Is(100);
+                semaphore.Release();
+            });
+
+            await semaphore.WaitAsync();
+        }
+    }
+}
